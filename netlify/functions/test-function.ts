e5ca069534166b16f780e64a41840afa9d4c951b
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-10-10 16:28:03 -0400

    stringifiying

--- netlify/functions/test-function.ts
+++ netlify/functions/test-function.ts
@@ -3,10 +3,9 @@ import type { Handler } from "@netlify/functions";
 
 const handler: Handler = async () => {
   // your server-side functionality
-
   return {
     statusCode:200, 
-    body: 'okkkkk'
+    body: JSON.stringify({ message: "ooookkkkkk" })
   }
 };
 
