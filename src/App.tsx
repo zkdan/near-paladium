f7ffa231bd1babb09cc2be87e36685db9eda93c9
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-10-10 16:17:14 -0400

    removing e for lint

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   const [res, setRes] = useState<string>('Nothing yet...');
   const question = 'write me a poem about grass.';
-  const handleClick =(e)=>{
+  const handleClick =()=>{
     axios('https://sage-lolly-8294fc.netlify.app/.netlify/functions/transcription-function', {params:{question}}).then(res =>{
         console.log(res);
         setRes('something!')
